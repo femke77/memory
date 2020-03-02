28984bd1dfab279b6abf488480122e7498052ad6
Meg Meyers <megan.g.meyers@gmail.com>
2020-03-01 16:51:32 -0800

    Updates

---
+++ precache-manifest.cab26986a5b10dedbc33347cc0e8e49a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8c64dc02929d0defc28cc15b256def",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "074380d69ca32a16e9f5",
+    "url": "/memory/static/css/main.2ac9dd6e.chunk.css"
+  },
+  {
+    "revision": "94d11e9210dd12e1ab99",
+    "url": "/memory/static/js/2.304e2be2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/memory/static/js/2.304e2be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074380d69ca32a16e9f5",
+    "url": "/memory/static/js/main.9d28a058.chunk.js"
+  },
+  {
+    "revision": "d95cbf321691194ee843",
+    "url": "/memory/static/js/runtime-main.00ccbe00.js"
+  }
+]);
