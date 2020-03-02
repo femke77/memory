a8ec2a14bc4c8c437bb38237a9c1cbce4c94c171
Meg Meyers <megan.g.meyers@gmail.com>
2020-03-01 16:38:37 -0800

    shuffle working

--- src/App.js
+++ src/App.js
@@ -17,14 +17,20 @@ class App extends Component {
 
   componentDidMount = () => {
     const images = imageLoader();
-    this.setState({ images: images });
+    this.setState({ images: images }, this.cb);
   }
 
+  cb = () => {
+    this.shuffleImages();
+  }
 
   shuffleImages = () => {
-    //logic to shuffle the images
-    //re-render will happen when state changes otherwise
-    //forceUpdate
+    const temp = this.state.images;
+    for (let i = temp.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [temp[i], temp[j]] = [temp[j], temp[i]];
+    }
+    this.setState({ images: temp })
   }
 
   handleClick = id => {
@@ -33,27 +39,22 @@ class App extends Component {
     //and if it is, we end the game, else continue
     if (this.state.clickedArr.includes(id)) {
       //if new score > top score, update top score
-      this.setState(() => {
-        const topScore = this.state.topScore > this.state.score ? this.state.topScore : this.state.score;
-
-        return {
-          message: "You guessed incorrectly!",
-          score: 0,
-          topScore: topScore,
-          clickedArr: []
-        }
+      this.setState({
+        message: "You guessed incorrectly!",
+        score: 0,
+        topScore: this.state.topScore > this.state.score ? this.state.topScore : this.state.score,
+        clickedArr: []
       });
     } else {
-      //add the id to the clickedArr and change the message
-
+      //add the id to the clickedArr and update the score
       this.setState({
         clickedArr: this.state.clickedArr.concat(id),
         message: "You guessed correctly!",
         score: this.state.score + 1
       });
     }
+    this.shuffleImages();
   }
-
 
   render() {
     return (
